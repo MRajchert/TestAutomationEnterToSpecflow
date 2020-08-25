b98fc4a48ea6a9f26b52c1aac81ddb2a2fba7f7e
Mikolaj Rajchert <mikolaj.rajchert@gfieast.com>
2020-08-25 17:37:03 +0200

    Update project before write scenario steps

---
+++ TestAutomationEnterToSpecflow/TestAutomationEnterToSpecflow/Feature/ContactToBlogFeature.feature
@@ -0,0 +1,9 @@
+﻿Feature: ContactToBlogFeature
+	In this test we have test have tests cases which checking
+
+@mytag
+Scenario: As User I enter to blog and I’m contacting by contact form.
+	Given I enter to "home" page
+	And I click on ”Contact” in menu
+	When I fill contact form
+	Then I expect to see message as „Message Sent (go back)”
---
+++ TestAutomationEnterToSpecflow/TestAutomationEnterToSpecflow/Steps/CheckContactFormSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace TestAutomationEnterToSpecflow.Steps
+{
+    [Binding]
+    public sealed class CheckContactFormSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public CheckContactFormSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I enter to ""(.*)"" page")]
+        public void GivenIEnterToPage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I click on ”Contact” in menu")]
+        public void GivenIClickOnContactInMenu()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I fill contact form")]
+        public void WhenIFillContactForm()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I expect to see message as „Message Sent \(go back\)”")]
+        public void ThenIExpectToSeeMessageAs()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
